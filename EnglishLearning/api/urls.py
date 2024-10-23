cf8e349059c3b30400f3735474c96087a268ea6a
ostowar.p.a <ostowar.p.a@gmail.com>
2024-10-23 22:57:55 +0330

    Permision with token
    permision for user on deck with token .....

--- EnglishLearning/api/urls.py
+++ EnglishLearning/api/urls.py
@@ -1,7 +1,16 @@
+# for get token:
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 from django.urls import path
 from . import views
 
 urlpatterns = [
+    # get token URL:
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("", views.getRoutes),
     path("decks/", views.getDecks),
     path("decks/<str:pk>/", views.getDeck),
--- EnglishLearning/api/views.py
+++ EnglishLearning/api/views.py
@@ -1,9 +1,10 @@
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 from decks.models import Deck, FlashCard
 from django.contrib.auth.models import User
 
-from .serializers import DeckSerializer, FlashCardSerializer, UserSerializer
+from .serializers import DeckSerializer, FlashCardSerializer
 
 
 @api_view(["GET"])
@@ -18,24 +19,30 @@ def getRoutes(request):
 
 # get Decks:
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getDecks(request):
-    decks = Deck.objects.all()
+    profile = request.user.profile
+    decks = profile.decks.all()
     serializer = DeckSerializer(decks, many=True)
     return Response(serializer.data)
 
 
 # get single Deck:
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getDeck(request, pk):
-    deck = Deck.objects.get(id=pk)
+    profile = request.user.profile
+    deck = profile.decks.get(id=pk)
     serializer = DeckSerializer(deck, many=False)
     return Response(serializer.data)
 
 
 # get cards of deck:
 @api_view(["GET"])
+@permission_classes([IsAuthenticated])
 def getCards(request, pk):
-    deck = Deck.objects.get(id=pk)
+    profile = request.user.profile
+    deck = profile.decks.get(id=pk)
     flashcards = deck.flashcards.all()
     serializer = FlashCardSerializer(flashcards, many=True)
     return Response(serializer.data)
@@ -47,7 +54,7 @@ def createUser(request):
     if User.objects.filter(username=data["username"]).exists():
         return Response({"error": "user is exists"}, status=400)
     elif User.objects.filter(email=data["email"]).exists():
-        return Response({"error": "email is exists"})
+        return Response({"error": "email is exists"}, status=400)
     else:
         user = User.objects.create_user(
             username=data["username"], email=data["email"], password=data["password"]
